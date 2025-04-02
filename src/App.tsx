ee9faa9cff94e07ffa2b6c3beabe369fc563ad73
AlbaCharlinSiweb <alba@siweb.es>
2025-04-02 11:34:46 +0200

    ultimos retoques

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,7 @@ import Block6 from './blocks/Block6';
 import Block7 from './blocks/Block7';
 import Block8 from './blocks/Block8';
 import Footer from './blocks/Footer';
+import Navbar from './blocks/Navbar';
 
 function App() {
 
@@ -18,6 +19,7 @@ function App() {
 
   return (
     <div className="flex flex-col justify-center items-center">
+      <Navbar />
       <Block1 handleClick={handleClick} />
       <Block2 />
       <Block3 />
